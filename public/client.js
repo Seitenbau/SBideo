02d14f79b33e3013e3ae4d5ce92e397809b80794
kompas <uaantony@gmail.com>
2017-10-13 20:39:50 +0200

    Format list, add search

--- public/client.js
+++ public/client.js
@@ -3,11 +3,17 @@ $.getJSON( "/data.json", function( obj ) {
   obj.items.forEach(function(category) {
       table += '<div class="col-sm-3 col-md-4"><h2>' + category.title + "</h2><ul>";
           category.items.forEach(function(event) {
-              table += "<li> " + event.meta.title;
+              table += "<li>" + event.meta.title;
               table += "<ul>";
               event.items.forEach(function(video) {
-                  table += "<li><a>" + video.meta.title + "</a>";
-                  table += "<div>meta</div></li>";
+                  table += "<li class='jsearch-row'><a class='jsearch-field'>" + video.meta.title + "</a>";
+                  video.meta.people.forEach(function(authors) {
+                      table += "<div>" + authors + "</div>";
+                  });
+                  video.meta.tags.forEach(function(tag) {
+                      table += "<span class='label label-primary'>" + tag + "</span>&nbsp;";
+                  });
+                  table += "<div>" + video.meta.description + "</div></li>";
               });
               table += "</ul>";
           });
