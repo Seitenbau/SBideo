717e5a62f77ab27d1eee78c36f0892e0f9ad7d25
Dennis Paulisch <dennis@Dennis-MBP.local>
2017-10-13 21:02:13 +0200

    add speaker

--- transcode.js
+++ transcode.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
 var chokidar = require('chokidar') //watcher
 var child_process = require('child_process');
 global.fileArray = []
@@ -26,8 +27,17 @@ function transcodeAndMoveFile(arrayIndex){
     log('File', path, 'has been added');
     var directoryToLoad = 'incoming';
     console.log("new file");
+    var metaJson = {
+      "title": fileName,
+      "description": "",
+      "tags": [],
+      "people": ["Tobias Schmidt"]
+    };
+    var json = JSON.stringify(metaJson); 
+    fs.writeFile('data/LightningTalks/2017-10-13/Hackathon/meta.json', json); 
+
     //child_process.exec('echo test: '+path+' :' , 
-    child_process.exec('for f in '+path+'; do ffmpeg -i "$f" -f mp4 -vcodec libx264 -preset medium -acodec aac -movflags faststart -vf scale=-1:720,format=yuv420p transcoded/'+fileName+'.mp4; mv "$f" "$f".encoded ; done',
+    child_process.exec('for f in '+path+'; do ffmpeg -i "$f" -f mp4 -vcodec libx264 -preset medium -acodec aac -movflags faststart -vf scale=-1:720,format=yuv420p data/LightningTalks/2017-10-13/Hackathon/video.mp4; mv "$f" "$f".encoded ; done',
     
     function (error, stdout, stderr) {  
       console.log('stdout: ' + stdout);
@@ -49,5 +59,6 @@ function transcodeAndMoveFile(arrayIndex){
           }
       }
     });
+
 }
 app.listen(3003);
