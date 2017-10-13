f19fb26577c4a3b80a0f24275bd7cf1797554725
Dennis Paulisch <dennis@Dennis-MBP.local>
2017-10-13 18:31:15 +0200

    add videoserver

---
+++ videoserver.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.htm'))
+})
+
+app.get('/stream/:mfile', (req, res) => {
+  const { mfile } = req.params;
+  const path = `./files/${mfile}`;
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
+app.listen(1337, function () {
+  console.log('SBideo Videostreamer Port 1337!')
+})
