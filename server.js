06facc16054298017532dee9f7af4ef4e7af76b2
Jakob Schroeter <jakob.schroeter@seitenbau.com>
2017-10-16 17:06:38 +0200

    express already supports range requests, removed code

--- server.js
+++ server.js
@@ -36,45 +36,8 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
-app.get('/stream', (req, res) => {
-  const { mfile } = req.param('file');
-console.log('req.params: '+req.param('file') );
-
-  const path = `${dataFolder}/`+req.param('file');
-console.log('path: '+ path );
-
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4'
-    }
-
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4'
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
-})
-
+// serve video files
+app.use('/data', express.static(__dirname + '/data'));
 
 app.use('/items.json', (req, res) => {
   var json = JSON.stringify(allItems);
@@ -156,7 +119,7 @@ watcher.on('add', function (filePath) {
     var videoFile = path.dirname(filePath) + '/video.mp4';
     if (fs.existsSync(videoFile)) {
       item.type = "video";
-      item.src = '/stream?file=' + path.relative(dataFolder, videoFile);
+      item.src = '/data/' + path.relative(dataFolder, videoFile);
     }
     var category = getCategoryByPath(relativeFilePath);
     addItemRecursive(allItems, category, item);
