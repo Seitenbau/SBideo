3368b5024bdd4080194b3c3a3e73ebf0efcb43de
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-02-10 23:24:33 +0100

    Show image instead of video poster
    better first impression on firefox

--- src/components/videoPlayer/index.js
+++ src/components/videoPlayer/index.js
@@ -6,13 +6,20 @@ export default class VideoPlayer extends Component {
     return (
       <div className={style.videoPlayer}>
         <div className={style.sizer}>
-          <video
-            className={style.video}
-            controls
-            autoPlay
-            poster="/assets/FuBK_testcard_vectorized.svg"
-            src={props.src ? props.src : ''}
-          />
+          {props.src && (
+            <video
+              className={style.video}
+              controls
+              autoPlay
+              poster="/assets/FuBK_testcard_vectorized.svg"
+              src={props.src}
+            />
+          )}
+          {!props.src && (
+            <span className={style.poster}>
+              <img src="/assets/FuBK_testcard_vectorized.svg" />
+            </span>
+          )}
         </div>
       </div>
     );
