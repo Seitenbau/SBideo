436e1d051096e8f4a7a1f1ff9edb320cc291813c
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-02-18 14:11:37 +0100

    Connect video player to redux store

--- src/components/videoPlayer/index.js
+++ src/components/videoPlayer/index.js
@@ -1,13 +1,19 @@
 import { h, Component } from 'preact';
 import style from './style.scss';
+import { connect } from 'preact-redux';
 
-export default class VideoPlayer extends Component {
+export class VideoPlayer extends Component {
   render(props) {
     return (
       <div className={props.className}>
         <div className={style.sizer}>
-          {props.src ? (
-            <video className={style.video} controls autoPlay src={props.src} />
+          {props.activeVideo.src ? (
+            <video
+              className={style.video}
+              controls
+              autoPlay
+              src={props.activeVideo.src}
+            />
           ) : (
             <div className={style.poster} />
           )}
@@ -16,3 +22,11 @@ export default class VideoPlayer extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    activeVideo: state.home.activeVideo
+  };
+};
+
+export default connect(mapStateToProps)(VideoPlayer);
