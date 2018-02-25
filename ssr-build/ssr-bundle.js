ee606416e72c1e8b51927c70cb8ba6ce3f03320d
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-02-25 21:32:02 +0100

    Updates

--- ssr-build/ssr-bundle.js
+++ ssr-build/ssr-bundle.js
@@ -4336,7 +4336,7 @@ var actions_setNewMetaInTree = function setNewMetaInTree(tree, newMeta) {
 };
 
 /* harmony default export */ var metaEditable_actions = (actions);
-// CONCATENATED MODULE: ./components/metaEditable/index.js
+// CONCATENATED MODULE: ../node_modules/preact-cli/lib/lib/webpack/dummy-loader.js!./components/metaEditable/index.js
 var metaEditable__extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 
