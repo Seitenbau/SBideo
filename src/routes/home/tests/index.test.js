70e842635cbb8b82b542b0c884e7b5266a2b6c06
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-02-25 21:27:04 +0100

    Fix pseudo test

--- src/routes/home/tests/index.test.js
+++ src/routes/home/tests/index.test.js
@@ -4,11 +4,10 @@ import { shallow } from 'preact-render-spy';
 jest.mock('../../../components/activeMetaContainer', () => <div />);
 
 describe('Test of the Homepage', () => {
-  test('Homepage renders the 3 main components', () => {
+  test('Homepage renders the folders component', () => {
     const context = shallow(
       <Home data={{ items: [] }} retrieveData={() => {}} />
     );
     expect(context.find('Folder').length).toBe(1);
-    expect(context.find('Connect').length).toBe(2); // VideoContainer, Search
   });
 });
