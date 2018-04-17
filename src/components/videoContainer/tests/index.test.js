59bffdbe4813d7bbb5c78f56d8c4a8d8f8a9f480
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-04-17 19:52:31 +0200

    Fix tests

--- src/components/videoContainer/tests/index.test.js
+++ src/components/videoContainer/tests/index.test.js
@@ -1,20 +1,30 @@
 import { h } from 'preact';
-import VideoContainer from '../index.js';
+import { VideoContainer } from '../index.js';
 import { shallow } from 'preact-render-spy';
+jest.mock('../../../components/activeMetaContainer', () => <div />);
+
+const activeVideoTest = { meta: { id: 'bla' } };
 
 describe('Test of the Video Container', () => {
-
   test('Video Container converts duration strings to correct integer value', () => {
-    const context = shallow(<VideoContainer startTime="2h21m34s" />);
+    const context = shallow(
+      <VideoContainer activeVideo={activeVideoTest} startTime="2h21m34s" />
+    );
     expect(context.state('currentTime')).toEqual(8494);
 
-    context.render(<VideoContainer startTime="2h34s" />);
+    context.render(
+      <VideoContainer activeVideo={activeVideoTest} startTime="2h34s" />
+    );
     expect(context.state('currentTime')).toEqual(7234);
 
-    context.render(<VideoContainer startTime="55m" />);
+    context.render(
+      <VideoContainer activeVideo={activeVideoTest} startTime="55m" />
+    );
     expect(context.state('currentTime')).toEqual(3300);
 
-    context.render(<VideoContainer startTime="43s" />);
+    context.render(
+      <VideoContainer activeVideo={activeVideoTest} startTime="43s" />
+    );
     expect(context.state('currentTime')).toEqual(43);
   });
 });
